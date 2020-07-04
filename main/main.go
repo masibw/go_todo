12f5201680ecb66daebff63aee512b2a8f82c590
masimacbook <masi19bw@gmail.com>
2020-07-04 14:10:50 +0900

    Docker-compose を設定

---
+++ main/main.go
@@ -0,0 +1,17 @@
+package main
+
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main(){
+	r:=gin.Default()
+	r.GET("/",func(c *gin.Context){
+		c.JSON(http.StatusOK,gin.H{
+			"message":"Hello World",
+		})
+	})
+	r.Run()
+}
